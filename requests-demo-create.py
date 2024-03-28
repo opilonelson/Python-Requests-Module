a5279488dc05212794eb56f770777752b93601f8
opilonelson <30505192+opilonelson@users.noreply.github.com>
2024-03-28 14:26:26 +0300

    Update requests-demo-create.py

--- requests-demo-create.py
+++ requests-demo-create.py
@@ -1,4 +1,5 @@
 import requests
+#relace with your actual PAT from github account
 headers = {"Authorization":"Bearer fffffffffffffffffffffff"}
 data = '{"name":"Python Requests Module", "description":"Repository for Python Requests module"}'
 resp=requests.post("https://api.github.com/user/repos", headers=headers, data=data)
