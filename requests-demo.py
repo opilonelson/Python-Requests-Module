60a7259f910bd9ddea1771bdc2fed504012c2bc2
opilonelson <30505192+opilonelson@users.noreply.github.com>
2024-03-28 14:31:43 +0300

    Update requests-demo.py

--- requests-demo.py
+++ requests-demo.py
@@ -1,6 +1,8 @@
 import requests
+#replace with your actual PAT from github account
 headers = {"Authorization":"Bearer ffffffffffffffffffffff"}
 data = '{"name":"Python Requests Module4"}'
+#replace username with your github account username
 resp=requests.patch("https://api.github.com/repos/username/Python-Requests-Module3", headers=headers, data =data)
 
 print(resp.text)
