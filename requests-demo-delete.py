da98a5840798f6ad4c3c00d0e771edcf513f6791
opilonelson <30505192+opilonelson@users.noreply.github.com>
2024-03-28 14:27:32 +0300

    Update requests-demo-delete.py

--- requests-demo-delete.py
+++ requests-demo-delete.py
@@ -1,6 +1,6 @@
 import requests
 headers = {"Authorization":"Bearer ffffffffffffffffffffffffffffff"}
-
+#replace username with your github account username
 resp=requests.delete("https://api.github.com/repos/username/Python-Requests-Mode", headers=headers)
 
 print(resp.text)
